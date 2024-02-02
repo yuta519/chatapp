737286e146deb6caf6f73f03ca451425a30a500d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-02 12:38:31 -0800

    Add name and email field on User and create seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+99.times.each do |_|
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+end
