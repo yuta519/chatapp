ff9c0c1bb585115f684ae70ca49093d114a3daeb
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-03 15:42:41 -0800

    Initialize Rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+# RSpec.describe 'User', type: :model do
+#   context 'who has vaild email and name' do # (almost) plain English
+#     it 'must be saved' do
+#       user = User.new
+#       user.email = Faker::Internet.email
+#       user.name = Faker::Internet.username
+#       user.save!
+
+#       expect(user.id).to be_present
+#     end
+#   end
+# end
