7929e00cddc9cf1f52ea994881e6876b998bda12
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-03 15:57:42 -0800

    Create test for user model successfuly saved

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'who has vaild email and name' do # (almost) plain English
+    it 'must be saved' do
+      user = User.new
+      user.email = 'user@localhost.com'
+      user.name = 'user'
+      user.save!
+
+      expect(user.id).to be_present
+    end
+  end
 end
-
-# RSpec.describe 'User', type: :model do
-#   context 'who has vaild email and name' do # (almost) plain English
-#     it 'must be saved' do
-#       user = User.new
-#       user.email = Faker::Internet.email
-#       user.name = Faker::Internet.username
-#       user.save!
-
-#       expect(user.id).to be_present
-#     end
-#   end
-# end
