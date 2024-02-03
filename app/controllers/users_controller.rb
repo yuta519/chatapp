a4267517b983af373860ffc57334396763fa47b1
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-03 12:08:22 -0800

    Create edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    return render 'edit', status: :unprocessable_entity unless @user.update(user_params)
+
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
