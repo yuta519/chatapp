fc78faffbef22ebacae4ed3f59641e7915df77cc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-03 16:25:01 -0800

    Modify test
    Using eq matcher for readability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'who has vaild email and name' do # (almost) plain English
-    it 'must be saved' do
-      user = User.new
-      user.email = 'user@localhost.com'
-      user.name = 'user'
+  context 'who has vaild email and name' do
+    subject(:user) { User.new }
+
+    it 'must be saved successfully' do
+      email = 'user@localhost.com'
+      name = 'user'
+
+      user.email = email
+      user.name = name
       user.save!
 
       expect(user.id).to be_present
+      expect(user.name).to eq(name)
+      expect(user.email).to eq(email)
     end
   end
 end
