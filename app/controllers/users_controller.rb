0007a0337b045b1f824b80e048a70c4216a6ef51
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-04 11:03:50 -0800

    Create new user path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def create; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.valid? && @user.save
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
 
   def show
     @user = User.find(params[:id])
