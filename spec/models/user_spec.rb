c0b076098e0ce3dfd22302d91f2961d02a227d53
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-04 10:49:34 -0800

    Add comprehensive test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,34 +2,82 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  let(:email) { 'user@localhost.com' }
-  let(:name) { 'user' }
+RSpec.describe User, type: :model do # rubocop:disable Metrics/BlockLength
+  describe 'validations' do # rubocop:disable Metrics/BlockLength
+    let(:email) { 'user@localhost.com' }
+    let(:name) { 'user' }
 
-  context 'with vaild email and name' do
-    subject(:target) { described_class.create({ name:, email: }) }
+    subject(:target) { described_class.new }
 
-    it 'must be valid' do
-      expect(target.valid?).to be_truthy
+    context 'with vaild email and name' do
+      before do
+        target.name = name
+        target.email = email
+      end
+
+      it 'must be valid' do
+        expect(target.valid?).to be_truthy
+      end
+
+      it 'must have no id' do
+        expect(target.id).to be_nil
+      end
+
+      it 'must have the same name' do
+        expect(target.name).to eq(name)
+      end
+
+      it 'must have the same email' do
+        expect(target.email).to eq(email)
+      end
+
+      it 'must be active' do
+        expect(target.active?).to be_truthy
+      end
     end
 
-    it 'must have an id' do
-      expect(target.id).to be_present
+    context 'with valid name and email is saved' do
+      before do
+        target.name = name
+        target.email = email
+        target.save
+      end
+      it 'must be valid' do
+        expect(target.valid?).to be_truthy
+      end
+
+      it 'must have no id' do
+        expect(target.id).to be_present
+      end
+
+      it 'must have the same name' do
+        expect(target.name).to eq(name)
+      end
+
+      it 'must have the same email' do
+        expect(target.email).to eq(email)
+      end
+
+      it 'must be active' do
+        expect(target.active?).to be_truthy
+      end
     end
 
-    it 'must have the same name' do
-      expect(target.name).to eq(name)
-    end
+    context 'with too short name' do
+      before do
+        target.name = 'a'
+        target.email = email
+        target.save
+      end
 
-    it 'must have the same email' do
-      expect(target.email).to eq(email)
-    end
+      it 'must be invalid' do
+        expect(target).to be_invalid
+      end
 
-    it 'must be active' do
-      expect(target.active?).to be_truthy
+      it 'must have a message about too short name' do
+        expect(target).to be_invalid
+        expect(target.errors.messages).to eq(name: ['is too short (minimum is 3 characters)'])
+      end
     end
-  end
-
-  context 'with invalid email' do
   end
 end
