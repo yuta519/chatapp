dae2785e998cec5e81708ccbd14dcc88a910f2ea
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-02-03 17:04:37 -0800

    Create the method, user.active?

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
 class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
+  def active?
+    disabled_at.nil?
+  end
 end
